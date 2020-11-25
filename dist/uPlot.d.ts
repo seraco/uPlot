76b26b99f0eff2ea07de47ba574915df32ecbb86
seraco <sebastiaramon_1994@hotmail.com>
2020-11-25 16:19:00 +0100

    remove band option

--- dist/uPlot.d.ts
+++ dist/uPlot.d.ts
@@ -391,9 +391,6 @@ declare namespace uPlot {
 			fill?: CanvasRenderingContext2D['fillStyle'];
 		};
 
-		/** any two adjacent series with band: true, are filled as a single low/high band */
-		band?: boolean;
-
 		/** line & legend color */
 		stroke?: CanvasRenderingContext2D['strokeStyle'];
 
